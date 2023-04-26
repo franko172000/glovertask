c29848072efaa62de24661ce91cdbe03eb1ff5cb
Franklin Anyaso <franko172000@gmail.com>
2023-04-26 10:23:43 +0100

    Added readme

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,37 +5,39 @@ namespace Database\Seeders;
 use App\Persistence\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class AdminSeeder extends Seeder
 {
-    private array $users = [
-        [
-            'email' => 'admin1@test.com',
-            'password' => 'test',
-            'first_name' => 'John',
-            'last_name' => 'Admin1',
-            'user_type' => 'admin'
-        ],
-        [
-            'email' => 'admin2@test.com',
-            'password' => 'test',
-            'first_name' => 'Fred',
-            'last_name' => 'Admin2',
-            'user_type' => 'admin'
-        ],
-        [
-            'email' => 'admin3@test.com',
-            'password' => 'test',
-            'first_name' => 'Kate',
-            'last_name' => 'Admin3',
-            'user_type' => 'admin'
-        ]
-    ];
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        collect($this->users)->each(fn($user)=>User::create($user));
+        $users = [
+            [
+                'email' => 'admin1@test.com',
+                'password' => Hash::make('test'),
+                'first_name' => 'John',
+                'last_name' => 'Admin1',
+                'user_type' => 'admin'
+            ],
+            [
+                'email' => 'admin2@test.com',
+                'password' => Hash::make('test'),
+                'first_name' => 'Fred',
+                'last_name' => 'Admin2',
+                'user_type' => 'admin'
+            ],
+            [
+                'email' => 'admin3@test.com',
+                'password' => Hash::make('test'),
+                'first_name' => 'Kate',
+                'last_name' => 'Admin3',
+                'user_type' => 'admin'
+            ]
+        ];
+        collect($users)->each(fn($user)=>User::create($user));
     }
 }
